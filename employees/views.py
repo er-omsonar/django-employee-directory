e4dc96824fd6b6ef634588006bf0a1ed39ca65b0
er-omsonar <er.omsonar@gmail.com>
2026-03-04 15:30:57 +0530

    Initial commit - Django Employee Directory

---
+++ employees/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from employees.models import Employee
+
+def employee_detail(request,pk):
+  employee = get_object_or_404(Employee,pk=pk)
+  context = {
+    'employee':employee,
+  }
+  return render(request,'employee_detail.html',context)
